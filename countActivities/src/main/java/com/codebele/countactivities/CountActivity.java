d140916d51e48585316fd76ae7d2dcd01ce5a7dd
nehakeerthi40 <keerthi@codebele.com>
2022-06-22 12:49:50 +0530

    count activities along with getcount method

--- countActivities/src/main/java/com/codebele/countactivities/CountActivity.java
+++ countActivities/src/main/java/com/codebele/countactivities/CountActivity.java
@@ -1,6 +1,9 @@
 package com.codebele.countactivities;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.Toast;
 
 public class CountActivity {
@@ -9,9 +12,25 @@ public class CountActivity {
     public static void IncrementCount(Context c) {
         count++;
         Toast.makeText(c, String.valueOf(count), Toast.LENGTH_SHORT).show();
+        SharedPreferences sp = c.getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("countValue", count);
+        editor.commit();
+
     }
     public static void DecrementCount(Context c){
         count--;
         Toast.makeText(c, String.valueOf(count), Toast.LENGTH_SHORT).show();
+        SharedPreferences sp = c.getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("countValue", count);
+        editor.commit();
     }
+    public static void getCount(Context c){
+        SharedPreferences sp = c.getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        int myIntValue = sp.getInt("countValue", -1);
+        Toast.makeText(c,String.valueOf(myIntValue),Toast.LENGTH_SHORT).show();
+    }
+
+
 }
